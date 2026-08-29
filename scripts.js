3b6c934d1e800692aa4cceb2ca110fb99fbe9f96
grace zhong <gracez10122@gmail.com>
2026-08-29 11:58:38 -0400

    fixes

--- scripts.js
+++ scripts.js
@@ -1,144 +1,206 @@
+
 const encounters = [
-  {
-      text: "Welcome to the bakery!"
-  },
+    {
+        text: "Welcome to the bakery!"
+    },
 
-  {
-      text: "Unfortunately, it's my first day on the job, so all I have is moldy bagels. Would you help me learn to make a cake?",
-      choices: [
-          {
-              text: "Sure, I'd love to help!",
-              result: "Great! Let's get started."
-          },
-          {
-              text: "Sorry, I'm not interested.",
-              result: "Wrong answer, pal! You're helping me make a cake."
-          }
-      ]
-  },
+    {
+        text: "Unfortunately, it's my first day on the job, so all I have is moldy bagels. Would you help me learn to make a cake?",
+        choices: [
+            {
+                text: "Sure, I'd love to help!",
+                result: "Great! Let's get started."
+            },
+            {
+                text: "Sorry, I'm not interested.",
+                result: "Wrong answer, pal! You're helping me make a cake."
+            }
+        ]
+    },
 
-  {
-      text: "I have no idea how to bake a cake, so I'm gonna need a recipe first."
-  },
+    {
+        text: "I have no idea how to bake a cake, so I'm gonna need a recipe first."
+    },
 
-  {
-    text: "We found it! It looks like a recipe for strawberry shortcake.",
-    choices: [
-        {
-            text: "Sounds yummy!",
-            result: "Great! I love strawberries too."
-        },
-        {
-            text: "Ewww! I hate strawberry shortcake :(",
-            result: "Well, that's too bad, because that's what we're gonna make!"
-        }
-    ]
-},
+    {
+        text: "We found it! It looks like a recipe for strawberry shortcake.",
+        choices: [
+            {
+                text: "Sounds yummy!",
+                result: "Great! I love strawberries too."
+            },
+            {
+                text: "Ewww! I hate strawberry shortcake :(",
+                result: "Well, that's too bad, because that's what we're gonna make!"
+            }
+        ]
+    },
 
-{
-    text: "Okay, let's read over the recipe first.",
-    choices: [
-        {
-            text: "Wait, I don't know how to bake, but it looks all wrong! Why does it say to put the strawberries on the cake before we even bake it??",
-            result: "Oh wait! You're right. I think we need to unscramble it!"
-        },
-        {
-            text: "Okay, looks good to me! Let's go to the supermarket and grab the ingredients.",
-            result: "No, dummy! It's obviously all wrong. We need to unscramble the steps first!"
-        }
-    ]
-},
+    {
+        text: "Okay, let's read over the recipe first.",
+        choices: [
+            {
+                text: "Wait, I don't know how to bake, but it looks all wrong! Why does it say to put the strawberries on the cake before we even bake it??",
+                result: "Oh wait! You're right. I think we need to unscramble it!"
+            },
+            {
+                text: "Okay, looks good to me! Let's go to the supermarket and grab the ingredients.",
+                result: "No, dummy! It's obviously all wrong. We need to unscramble the steps first!"
+            }
+        ]
+    },
 
+    {
+        text: "Perfecto! Now that the recipe is in order, let's head to the supermarket to get the ingredients."
+    },
 
-{
-    text: "Perfecto! Now that the recipe is in order, let's head to the supermarket to get the ingredients."
-},
+    {
+        text: "After a long ride. . ."
+    },
 
-{
-    text: "After a long ride. . ."
-},
+    {
+        text: "Okay, we made it to the supermarket, but now we need to find all the ingredients."
+    },
 
-{
-    text: "Okay, we made it to the supermarket, but now we need to find all the ingredients."
-},
+    {
+        text: "I made a shopping list with all the ingredients and how much of each we need.",
+        choices: [
+            {
+                text: "Yay! I'm ready.",
+                result: "Great! Let's start with the dairy section."
+            },
+            {
+                text: "I'm so tired after the ride though... Can we take a break first?",
+                result: "Awwww, that's too bad for you. We need to get started, lazybones!"
+            }
+        ]
+    },
 
-{
-    text: "I made a shopping list with all the ingredients and how much of each we need.",
-    choices: [
-        {
-            text: "Yay! I'm ready.",
-            result: "Great! Let's start with the dairy section."
-        },
-        {
-            text: "I'm so tired after the ride though... Can we take a break first?",
-            result: "Awwww, that's too bad for you. We need to get started, lazybones!"
-        }
-    ]
-},
+    {
+        text: "Hmmm, where could the dairy section be?"
+    },
 
-{
-    text: "Hmmm, where could the dairy section be?"
-},
+    {
+        text: "We found the dairy section! Now, let's check the ingredients on the list to see what we need for here.",
+        choices: [
+            {
+                text: "We need eggs, milk, butter, heavy cream, half-and-half.",
+                result: "Yup! Sounds right."
+            },
+            {
+                text: "We need flour, sugar, baking powder, vanilla extract, powdered sugar.",
+                result: "Wrong! Those are all in the baking section."
+            }
+        ]
+    },
 
-// Code for finding the dairy section
+    {
+        text: "Wow, this fridge door is so hard to open!"
+    },
 
-{
-    text: "We found the dairy section! Now, let's check the ingredients on the list to see what we need for here.",
-    choices: [
-        {
-            text: "We need eggs, milk, butter, heavy cream, half-and-half.",
-            result: "Yup! Sounds right."
-        },
-        {
-            text: "We need flour, sugar, baking powder, vanilla extract, powdered sugar.",
-            result: "Wrong, stupid! Those are all in the baking section. The eggs, milk, butter, heavy cream, half-and-half should be here."
-        }
-    ]
-},
+    {
+        text: "Awesome! We got all the ingredients from here."
+    },
 
-{
-    text: "Wow, this fridge door is so hard to open!"
-},
+    {
+        text: "Now, we gotta head to the produce section to find the strawberries."
+    },
 
-// Code for opening the fridge door
+    {
+        text: "Okay, help me find the best strawberries here. No moldy fruits allowed!"
+    },
 
-{
-    text: "Awesome! We got all the ingredients from here."
-},
+    {
+        text: "Great! We're almost done. We just gotta get to the baking aisle for the rest of the ingredients."
+    },
 
-{
-    text: "Now, we gotta head to the produce section to find the strawberries."
-},
+    {
+        text: "Oh no! So many of the ingredients are on the top shelf! I can't reach them. We're gonna need to jump!"
+    },
 
-{
-    text: "Okay, help me find the best strawberries here. No moldy fruits allowed!"
-},
+    {
+        text: "Awesome! We got all the ingredients we need. Now, let's head back to the bakery and start baking!"
+    },
 
-// Code to choose the best strawberries
-
-{
-    text: "Great! We're almost done. We just gotta get to the baking aisle for the rest of the ingredients."
-},
-
-{
-    text: "Oh no! So many of the ingredients are on the top shelf! I can't reach them. We're gonna need to jump!"
-},
-
-// Code for jumping to reach the ingredients
-
-{
-    text: "Awesome! We got all the ingredients we need. Now, let's head back to the bakery and start baking!"
-},
-
-{
-    text: "After the ride back. . ."
-}
+    {
+        text: "After the ride back. . ."
+    }
 ];
 
 
 let currentEncounter = 0;
 
 const screen = document.getElementById("screen");
+
+
+function showEncounter() {
+
+    const encounter = encounters[currentEncounter];
+
+    let html = `
+        <p>${encounter.text}</p>
+    `;
+
+    if (encounter.choices) {
+
+        encounter.choices.forEach((choice, index) => {
+
+            html += `
+                <button onclick="choose(${index})">
+                    ${choice.text}
+                </button>
+            `;
+
+        });
+
+    } else {
+
+        html += `
+            <button onclick="next()">Next</button>
+        `;
+
+    }
+
+    screen.innerHTML = html;
+}
+
+
+function choose(choiceIndex) {
+
+    const encounter = encounters[currentEncounter];
+
+    const choice = encounter.choices[choiceIndex];
+
+    screen.innerHTML = `
+        <p>${choice.result}</p>
+        <button onclick="next()">Next</button>
+    `;
+}
+
+
+function next() {
+
+    currentEncounter++;
+
+    if (currentEncounter < encounters.length) {
+
+        showEncounter();
+
+    } else {
+
+        screen.innerHTML = `
+            <h1>End of this section!</h1>
+            <p>More bakery adventures coming soon...</p>
+        `;
+
+    }
+}
+
+
+
+showEncounter();
+
+
 
 function showEncounter() {
   const encounter = encounters[currentEncounter];
