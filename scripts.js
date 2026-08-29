6be4dcfc3873a159b3a58c05b8493d4aa14e28fc
grace zhong <gracez10122@gmail.com>
2026-08-29 09:59:53 -0400

    some script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-body {
-
+const encounter = {
+    text: "Welcome to the bakery!",
+    text: "Unfortunately, it's my first day on the job so all I have is moldy bagels. Would help me learn to make a cake?",
+    choices: [
+        {
+            text: "Sure, I'd love to help!",
+            result: "Great! let's get started. What do we need first?"
+        },
+        {
+            text: "Sorry, I'm not interested.",
+            result: "Wrong answer pal! You're helping me make a cake."
+        }
+    ]
 }
