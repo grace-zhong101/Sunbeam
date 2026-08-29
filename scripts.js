0daed108edbbc8b73c8145e91c1dcf0681ec723b
kayla <kaylazhang37@gmail.com>
2026-08-29 10:38:56 -0400

    more script until recipe unscrambling

--- scripts.js
+++ scripts.js
@@ -11,4 +11,35 @@ const encounter = {
             result: "Wrong answer pal! You're helping me make a cake."
         }
     ]
+    text: "I have no idea how to bake a cake, so I’m gonna need a recipe first. Lets look around the kitchen to see if there’s one around."
+    
+    //code for pressing recipe in hidden in kitchen
+
+    text: "We found it! It looks like a recipe for strawberry shortcake."
+    choices: [
+        {
+            text: "Sounds yummy!",
+            result: "Great! I love strawberries too."
+        }
+        {
+            text: "Ewww! I hate strawberry shortcake :(",
+            result: "Well that's too bad, because that's what we're gonna make!"
+        }
+    ]
+    text: "Okay, let's read over the recipe first."
+    choices: [
+        {
+            text: "Wait, I don't know how to bake, but it looks all wrong! Why does it say to put the strawberries on the cake before we even bake it??"
+            result: "Oh wait! You're right. I think we need to unscramble it!"
+        }
+        {
+            text: "Okay, looks good to me! Let's go to the supermarket and grab the ingredients."
+            result: "No, dummy! It's obviously all wrong. We need to unscramble the steps first!"
+
+        }
+    ]
+
+    //code for unscrambling the recipe
+
+
 }
